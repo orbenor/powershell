db3f919717751f18cfe50760cd8aa99366ede614
orbenor <orbenor@gmail.com>
2022-01-19 19:07:24 +0200

    Update dhcp-report.ps1

--- Microsoft-dhcp/dhcp-report.ps1
+++ Microsoft-dhcp/dhcp-report.ps1
@@ -5,4 +5,4 @@ Invoke-Command -ComputerName "domain01" -Credential $creds -ScriptBlock { Get-Dh
 Invoke-Command -ComputerName "domain01" -Credential $creds -ScriptBlock { Get-DhcpServerv4Scope | Select-Object -Property ScopeId | ft -hide } | out-file -filepath /root/powershell/dhcp-scope.csv
 write-host $A
 # FREE ip in DOMAIN
-Invoke-Command -ComputerName "domain01" -Credential $creds -ScriptBlock { Get-DhcpServerv4Scope | select ScopeID | ForEach-Object { Get-DhcpServerv4FreeIPAddress -ScopeId $_.ScopeId -NumAddress 1024 -ErrorAction SilentlyContinue }}
+Invoke-Command -ComputerName "domain01" -Credential $creds -ScriptBlock { Get-DhcpServerv4Scope | select ScopeID | ForEach-Object { Get-DhcpServerv4FreeIPAddress -ScopeId $_.ScopeId -NumAddress 1024 }}
