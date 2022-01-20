33b1f84f565a534ba52a391a36ac6dfa14caa3df
orbenor <orbenor@gmail.com>
2022-01-20 12:56:26 +0200

    Update dhcp-report.ps1

--- Microsoft-dhcp/dhcp-report.ps1
+++ Microsoft-dhcp/dhcp-report.ps1
@@ -1,8 +1,9 @@
+# Read password from Secure file
 $creds = Import-Clixml -Path /root/powershell/benorpass.cred
-# Scope list in DHCP server.
-Invoke-Command -ComputerName "domain01" -Credential $creds -ScriptBlock { Get-DhcpServerv4Scope | Select-Object -Property Name, ScopeId, SubnetMask, StartRange, EndRange, Description | ft -Autosize }
 
-Invoke-Command -ComputerName "domain01" -Credential $creds -ScriptBlock { Get-DhcpServerv4Scope | Select-Object -Property ScopeId | ft -hide } | out-file -filepath /root/powershell/dhcp-scope.csv
-write-host $A
-# FREE ip in DOMAIN
-Invoke-Command -ComputerName "domain01" -Credential $creds -ScriptBlock { Get-DhcpServerv4Scope | select ScopeID | ForEach-Object { Get-DhcpServerv4FreeIPAddress -ScopeId $_.ScopeId -NumAddress 1024 }}
+# Scope list in DHCP server.
+Invoke-Command -ComputerName "DOMAIN" -Credential $creds -ScriptBlock { Get-DhcpServerv4Scope | Select-Object -Property Name, ScopeId, SubnetMask, StartRange, EndRange, Description | ft -Autosize }
+
+# Get the list of all free IP's in each Scope.
+Invoke-Command -ComputerName "DOMAIN" -Credential $creds -ScriptBlock { Get-DhcpServerv4Scope | select ScopeID | ForEach-Object { Write-Host "-----------------"; Write-Host "Scope", $_.ScopeId; Write-Host "-----------------"; Get-DhcpServerv4FreeIPAddress -ScopeId $_.ScopeId -NumAddress 1024 -ea silentlyContinue -WarningAction silentlyContinue }}
+
