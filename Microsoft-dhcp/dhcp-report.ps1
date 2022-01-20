e8448f6a4ee13da44c9fb47f270ed73a87ae2c23
orbenor <orbenor@gmail.com>
2022-01-20 13:28:43 +0200

    Update dhcp-report.ps1

--- Microsoft-dhcp/dhcp-report.ps1
+++ Microsoft-dhcp/dhcp-report.ps1
@@ -7,3 +7,6 @@ Invoke-Command -ComputerName "DOMAIN" -Credential $creds -ScriptBlock { Get-Dhcp
 # Get the list of all free IP's in each Scope.
 Invoke-Command -ComputerName "DOMAIN" -Credential $creds -ScriptBlock { Get-DhcpServerv4Scope | select ScopeID | ForEach-Object { Write-Host "-----------------"; Write-Host "Scope", $_.ScopeId; Write-Host "-----------------"; Get-DhcpServerv4FreeIPAddress -ScopeId $_.ScopeId -NumAddress 1024 -ea silentlyContinue -WarningAction silentlyContinue }}
 
+# Get the list of all free IP's in each Scope and Limt the search in the scope.
+Invoke-Command -ComputerName "DOMAIN" -Credential $creds -ScriptBlock { Get-DhcpServerv4FreeIPAddress -ScopeId 192.168.113.0 -NumAddress 20 -StartAddress 192.168.113.51 -EndAddress 192.168.113.100 }
+Invoke-Command -ComputerName "DOMAIN" -Credential $creds -ScriptBlock { Get-DhcpServerv4FreeIPAddress -ScopeId 192.168.115.0 -EndAddress 132.76.115.20 -NumAddress 10 -StartAddress 192.168.115.100 }
