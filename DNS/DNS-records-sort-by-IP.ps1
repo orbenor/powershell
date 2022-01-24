1e37b4897fc2bc82c0ee5d44040f3a13c3ac0ae6
orbenor <orbenor@gmail.com>
2022-01-24 16:47:08 +0200

    Delete DNS-records-sort-by-IP.ps1

--- DNS/DNS-records-sort-by-IP.ps1
+++
@@ -1,5 +0,0 @@
-#Get-DnsServerResourceRecord -ZoneName "example.com" | Select-Object -Property HostName, {$_.RecordData.Ipv4address} | sort-object -Property {$_.RecordData.Ipv4address -as [Version]} | ft # | Sort-Object -Property {$_.IPv4Address -as [Version]} | sort-object -Property {$_ -as [Version] } | ft
-
-Get-DnsServerResourceRecord -ZoneName "example.com" -RRType "A" | select HostName,RecordType,Timestamp,TimeToLive,@{Name='RecordData';Expression={$_.RecordData.IPv4Address.ToString()}} | sort-object -Property { $_.RecordData -as [Version] }| ft -autosize
-# HostName                  RecordType Timestamp            TimeToLive      RecordData
-# --------                  ---------- ---------            ----------      ----------
