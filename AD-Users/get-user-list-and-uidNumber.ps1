14980ea5c19a3e1b01b9d36b8a6a5d5e5b7827a4
orbenor <orbenor@gmail.com>
2022-02-02 17:35:35 +0200

    Nice sort and table view

--- AD-Users/get-user-list-and-uidNumber.ps1
+++ AD-Users/get-user-list-and-uidNumber.ps1
@@ -1,2 +1,4 @@
 Get-ADUser -filter * -Properties * | select SamAccountName,uidNumber | sort -Property SamAccountName
 Get-ADUser -filter * -Properties * | select SamAccountName,uidNumber | sort -Property uidNumber
+
+Get-ADUser -Filter * -Properties SamAccountName,uidNumber | Select-Object SamAccountName,uidNumber | Where-Object {$_.uidNumber -ne $null} | sort uidNumber  |ft -AutoSize
